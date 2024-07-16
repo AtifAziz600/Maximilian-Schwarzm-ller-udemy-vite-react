dc44425d5593e3af4fa92a676b3b644420e3db9b
AtifAziz600 <feralligatrfahim630@gmail.com>
2024-07-16 13:15:20 +0600

    'h'

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -14,12 +14,22 @@ function PostsList({ isPosting, onStopPosting }) {
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no post</h2>
+          <p>Start adding one</p>
+        </div>
+      )}
     </>
   );
 }
